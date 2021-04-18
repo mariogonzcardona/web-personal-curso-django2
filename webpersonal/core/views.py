29034ac91e382f5430c5adc31dd0332100b46064
Mario Gonzalez <alejandrogonzalez@uadec.edu.mx>
2021-04-17 23:47:05 -0500

    Actualizacion de datos

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render,HttpResponse
 
+html_base="""
+    <h1>Mi Web Personal</h1>
+    <ul>
+        <li><a href="/">Portada</a></li>
+        <li><a href="/about-me/">Acerca de</a></li>
+        <li><a href="/portfolio/">Portafolio</a></li>
+        <li><a href="/contact/">Contacto</a></li>
+    </ul>
+"""
 # Create your views here.
 def home(request):
-    return render(request,"core/home.html")
+    return render(request,'core/home.html')
 
 def about(request):
-    return render(request,"core/about.html")
+    return render(request,'core/about.html')
 
+# Aqui iba portfolio pero se movio a su respectiva app de portfolio
 
 def contact(request):
-    return render(request,"core/contact.html")
+    return render(request,'core/contact.html')
--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -1,18 +1,24 @@
 from django.db import models
+from django.db.models import fields
 
 # Create your models here.
-class Project(models.Model):
-    title=models.CharField(max_length=200,verbose_name="Titulo")
+class Project (models.Model):
+    title=models.CharField(max_length=250,verbose_name="Titulo")
     description=models.TextField(verbose_name="Descripcion")
     image=models.ImageField(verbose_name="Imagen",upload_to="projects")
-    link=models.URLField(null=True,blank=True, verbose_name="Direccion Web")
+    link=models.URLField( null=True,blank=True,verbose_name="Enlace")
     created=models.DateTimeField(auto_now_add=True,verbose_name="Fecha de Creacion")
-    updated=models.DateTimeField(auto_now=True,verbose_name="Fecha de Edicion")
+    updated=models.DateTimeField(auto_now=True,verbose_name="Fecha de Actualizacion")
     
-    class Meta:
+    #Metadata
+    class Meta :
         verbose_name="proyecto"
         verbose_name_plural="proyectos"
-        ordering=["-created"]
-        
+        ordering = ['-created']
+
+    # #Methods
+    # def get_absolute_url(self):
+    #     return reverse('url', args=[args])
+
     def __str__(self):
         return self.title
